198bdf5e756ee6ca1b69e91ac0defc16c7df7e33
JensLegarthRyge <jens.legarth.ryge@gmail.com>
2023-03-23 20:12:13 +0100

    established package structure + creation of Note-model

--- app/src/main/java/com/example/notes_app_android/activities/MainActivity.kt
+++ app/src/main/java/com/example/notes_app_android/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.notes_app_android
+package com.example.notes_app_android.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.notes_app_android.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/notes_app_android/model/Note.kt
@@ -0,0 +1,4 @@
+package com.example.notes_app_android.model
+
+class Note(private var title: String, private var body: String) {
+}
